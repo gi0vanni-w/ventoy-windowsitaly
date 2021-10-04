bd71552a421a05dac9cb20c532ea18b28b4bc93d
Giovanni <70816356+gi0vanni-w@users.noreply.github.com>
2021-10-04 16:08:01 +0200

    Add files via upload

---
+++ Start.bat
@@ -0,0 +1,1 @@
+@powershell.exe -NoProfile -ExecutionPolicy Unrestricted -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Unrestricted -File ""%~dp0ventoy.ps1""' -Verb RunAs}";
---
+++ ventoy.ps1
@@ -0,0 +1,15 @@
+$source = "https://github.com/ventoy/Ventoy/releases/download/v1.0.53/ventoy-1.0.53-windows.zip"
+$destination = "ventoy.zip"
+
+Start-BitsTransfer -Source $source -Destination $destination
+
+Expand-Archive -Path "ventoy.zip" -DestinationPath "ventoy-win"
+
+Remove-Item -path "ventoy.zip"
+
+cd ventoy-win
+
+cd ventoy-1.0.53
+
+.\Ventoy2Disk.exe
+
