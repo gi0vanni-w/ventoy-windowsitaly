d7c0ed151852db7c9c690244534d975debebaafb
Giovanni <70816356+gi0vanni-w@users.noreply.github.com>
2021-10-06 22:46:23 +0200

    Delete Start.bat

--- Start.bat
+++
@@ -1,1 +0,0 @@
-@powershell.exe -NoProfile -ExecutionPolicy Unrestricted -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Unrestricted -File ""%~dp0ventoy.ps1""' -Verb RunAs}";
